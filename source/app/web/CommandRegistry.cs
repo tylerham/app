cf94d2ae0288fd671a61d7ed0c04f94f2c87ac0b
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-11-21 14:47:02 -0700

    Updated

--- source/app/web/CommandRegistry.cs
+++ source/app/web/CommandRegistry.cs
@@ -5,16 +5,16 @@ namespace app.web
 {
   public class CommandRegistry : IFindCommands
   {
-      private readonly IEnumerable<IProcessOneRequest> _processors;
+    readonly IEnumerable<IProcessOneRequest> processors;
 
-      public CommandRegistry(IEnumerable<IProcessOneRequest> processors )
-      {
-          _processors = processors;
-      }
+    public CommandRegistry(IEnumerable<IProcessOneRequest> processors)
+    {
+      this.processors = processors;
+    }
 
-      public IProcessOneRequest get_the_command_that_can_process(IContainRequestDetails request)
-      {
-        return  _processors.First(c => c.can_process(request));
-      }
+    public IProcessOneRequest get_the_command_that_can_process(IContainRequestDetails request)
+    {
+      return processors.First(c => c.can_process(request));
+    }
   }
 }
