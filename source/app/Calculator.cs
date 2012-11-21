abc99e6c9700b2c133f8b0d2fd65deecdc535b4b
Tyler Hamilton <tylerhamilton@gmail.com>
2012-11-21 10:47:32 -0800

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,9 +1,13 @@
-﻿namespace app
+﻿using System;
+
+namespace app
 {
   public class Calculator
   {
     public int add(int i, int i1)
     {
+      if(i1 < 0)
+        throw new ArgumentException("Can not add negative number to positive.", "i1");
         return i + i1;
     }
   }
