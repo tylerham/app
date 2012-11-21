fbc39734277c0fb3e791bdadc107cfb96ef13a08
Tyler Hamilton <tylerhamilton@gmail.com>
2012-11-21 13:44:47 -0800

    Pushing new changes

--- source/app/web/CommandRegistry.cs
+++ source/app/web/CommandRegistry.cs
@@ -1,10 +1,17 @@
-﻿namespace app.web
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace app.web
 {
   public class CommandRegistry : IFindCommands
   {
-    public IProcessOneRequest get_the_command_that_can_process(IContainRequestDetails request)
-    {
-      throw new System.NotImplementedException();
+    readonly IEnumerable<IProcessOneRequest> _commands;
+
+    public CommandRegistry(IEnumerable<IProcessOneRequest> commands) {
+      _commands = commands;
     }
+
+    public IProcessOneRequest get_the_command_that_can_process(IContainRequestDetails request) { return 
+      _commands.LastOrDefault(c => c.can_process(request)); }
   }
 }
