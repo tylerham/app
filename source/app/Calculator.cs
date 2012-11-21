652e4bcec4e236216dce01514b727b9c395a664c
dspeidel <devons@iqmetrix.com>
2012-11-21 10:53:18 -0800

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,14 +1,23 @@
 ﻿using System;
+using System.Data;
 
 namespace app
 {
   public class Calculator
   {
-    public int add(int i, int i1)
+  	private readonly IDbConnection _connection;
+
+  	public Calculator(IDbConnection connection)
+  	{
+  		_connection = connection;
+  	}
+
+  	public int add(int i, int i1)
     {
         if (i <0 || i1 < 0)
             throw new ArgumentException("Dumb calculator cannot add negative numbers. Maybe try the smart calculator?");
 
+		_connection.Open();
         return i + i1;
     }
   }
