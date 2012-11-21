fd07a1e9f0ae71f50f8aa114ab0282ec1e799ba5
Tyler Hamilton <tylerhamilton@gmail.com>
2012-11-21 13:33:33 -0800

    Pushing new changes

--- source/app/web/FrontController.cs
+++ source/app/web/FrontController.cs
@@ -2,9 +2,15 @@
 {
   public class FrontController : IProcessRequests
   {
+    readonly IFindCommands _commandRegistry;
+
+    public FrontController(IFindCommands command_registry) {
+      _commandRegistry = command_registry;
+    }
+
     public void process(IContainRequestDetails a_new_controller_request)
     {
-      throw new System.NotImplementedException();
+      _commandRegistry.get_the_command_that_can_process(a_new_controller_request).run(a_new_controller_request);
     }
   }
 }
