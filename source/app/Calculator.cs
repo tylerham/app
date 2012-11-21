cb04878a98ba760ebca8b2b305b2815cf00effa8
Tyler Hamilton <tylerhamilton@gmail.com>
2012-11-21 11:01:06 -0800

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -5,20 +5,20 @@ namespace app
 {
   public class Calculator
   {
-  	private readonly IDbConnection _connection;
+    readonly IDbConnection connection;
 
-  	public Calculator(IDbConnection connection)
-  	{
-  		_connection = connection;
-  	}
+    public Calculator(IDbConnection connection) { this.connection = connection; }
 
-  	public int add(int i, int i1)
+    public int add(int i, int i1)
     {
-        if (i <0 || i1 < 0)
-            throw new ArgumentException("Dumb calculator cannot add negative numbers. Maybe try the smart calculator?");
+      if (i < 0 || i1 < 0)
+        throw new ArgumentException("Dumb calculator cannot add negative numbers. Maybe try the smart calculator?");
 
-		_connection.Open();
-        return i + i1;
+      connection.Open();
+      var command = connection.CreateCommand();
+      command.ExecuteNonQuery();
+      connection.Dispose();
+      return i + i1;
     }
   }
 }
