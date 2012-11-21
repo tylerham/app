838ef5aecf83f10e1a7fac078dee8f30d9a5ca70
ChristopherWolf <christopherw@iqmetrix.com>
2012-11-21 13:47:38 -0800

    Pushing new changes

--- source/app/web/CommandRegistry.cs
+++ source/app/web/CommandRegistry.cs
@@ -3,18 +3,22 @@ using System.Linq;
 
 namespace app.web
 {
-  public class CommandRegistry : IFindCommands
-  {
-    readonly IEnumerable<IProcessOneRequest> processors;
+	public class CommandRegistry : IFindCommands
+	{
+		readonly IEnumerable<IProcessOneRequest> processors;
+		readonly ICreateTheCommandWhenOneCantBeFound _specialCommand;
 
-    public CommandRegistry(IEnumerable<IProcessOneRequest> processors)
-    {
-      this.processors = processors;
-    }
+		public CommandRegistry(IEnumerable<IProcessOneRequest> processors, ICreateTheCommandWhenOneCantBeFound specialCommand)
+		{
+			this.processors = processors;
+			_specialCommand = specialCommand;
+		}
 
-    public IProcessOneRequest get_the_command_that_can_process(IContainRequestDetails request)
-    {
-      return processors.First(c => c.can_process(request));
-    }
-  }
+		public IProcessOneRequest get_the_command_that_can_process(IContainRequestDetails request)
+		{
+			var match = processors.FirstOrDefault(c => c.can_process(request));
+
+			return match ?? _specialCommand();
+		}
+	}
 }
