24e0b67ced885b37fd93db62f71edb1bb5d2c03b
Keivan Beigi <keivanb@iqmetrix.com>
2012-11-21 13:43:40 -0800

    Pushing new changes

--- source/app/web/CommandRegistry.cs
+++ source/app/web/CommandRegistry.cs
@@ -1,10 +1,20 @@
-﻿namespace app.web
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace app.web
 {
   public class CommandRegistry : IFindCommands
   {
-    public IProcessOneRequest get_the_command_that_can_process(IContainRequestDetails request)
-    {
-      throw new System.NotImplementedException();
-    }
+      private readonly IEnumerable<IProcessOneRequest> _processors;
+
+      public CommandRegistry(IEnumerable<IProcessOneRequest> processors )
+      {
+          _processors = processors;
+      }
+
+      public IProcessOneRequest get_the_command_that_can_process(IContainRequestDetails request)
+      {
+        return  _processors.First(c => c.can_process(request));
+      }
   }
 }
