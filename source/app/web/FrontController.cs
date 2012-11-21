a90acd65146d87ee05e8322ae81e2a3762df58b5
igor-toporet <igort@iqmetrix.com>
2012-11-21 13:33:32 -0800

    Pushing new changes

--- source/app/web/FrontController.cs
+++ source/app/web/FrontController.cs
@@ -2,9 +2,16 @@
 {
   public class FrontController : IProcessRequests
   {
-    public void process(IContainRequestDetails a_new_controller_request)
+      private readonly IFindCommands _commandRegistry;
+
+      public FrontController(IFindCommands commandRegistry)
+      {
+          _commandRegistry = commandRegistry;
+      }
+
+      public void process(IContainRequestDetails a_new_controller_request)
     {
-      throw new System.NotImplementedException();
+      _commandRegistry.get_the_command_that_can_process(a_new_controller_request).run(a_new_controller_request);
     }
   }
 }
