40d79e2a1b15fee7d496c8b7a718c24a8d448009
Tyler Hamilton <tylerhamilton@gmail.com>
2012-11-21 10:56:23 -0800

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,15 +1,22 @@
 ﻿using System;
+using System.Data;
 
 namespace app
 {
   public class Calculator
   {
+    readonly IDbConnection database;
+
+    public Calculator(IDbConnection db) { database = db; }
+
     public int add(int i, int i1)
     {
-        if (i <0 || i1 < 0)
-            throw new ArgumentException("Dumb calculator cannot add negative numbers. Maybe try the smart calculator?");
+      if (i < 0 || i1 < 0)
+        throw new ArgumentException("Dumb calculator cannot add negative numbers. Maybe try the smart calculator?");
+      database.Open();
+      database.Dispose();
 
-        return i + i1;
+      return i + i1;
     }
   }
 }
