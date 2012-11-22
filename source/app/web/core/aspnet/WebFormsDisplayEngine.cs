0f3133e83ea4bfd7b46490946d6e3e770449ae44
Tyler Hamilton <tylerhamilton@gmail.com>
2012-11-22 09:57:22 -0800

    Pushing new changes

--- source/app/web/core/aspnet/WebFormsDisplayEngine.cs
+++ source/app/web/core/aspnet/WebFormsDisplayEngine.cs
@@ -1,10 +1,21 @@
-﻿namespace app.web.core.aspnet
+﻿using System.Web;
+
+namespace app.web.core.aspnet
 {
   public class WebFormsDisplayEngine : IDisplayInformation
   {
+    readonly ICreateViews _viewFactory;
+    readonly HttpContext _request;
+
+    public WebFormsDisplayEngine(ICreateViews view_factory, HttpContext request)
+    {
+      _viewFactory = view_factory;
+      _request = request;
+    }
+
     public void display<PresentationModel>(PresentationModel model)
     {
-      throw new System.NotImplementedException();
+_viewFactory.create_view_that_can_render(model).ProcessRequest(_request);
     }
   }
 }
