6217d8cf45ba78390cc9e5ee158d4464a3f773b6
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-11-22 15:07:17 -0700

    Updated

---
+++ source/app.specs/ContainerSpecs.cs
@@ -0,0 +1,28 @@
+﻿ using Machine.Specifications;
+ using app.utility.service_locator;
+ using developwithpassion.specifications.rhinomocks;
+ using developwithpassion.specifications.extensions;
+
+namespace app.specs
+{  
+  [Subject(typeof(Container))]  
+  public class ContainerSpecs
+  {
+    public abstract class concern : Observes<IFindDependencies,
+                                      Container>
+    {
+        
+    }
+
+   
+    public class when_observation_name : concern
+    {
+
+      Because b = () =>
+        result = sut.an<>()
+
+      It first_observation = () =>        
+        
+    }
+  }
+}
---
+++ source/app/utility/service_locator/Container.cs
@@ -0,0 +1,10 @@
+﻿namespace app.utility.service_locator
+{
+  public class Container :IFindDependencies
+  {
+    public TDependency an<TDependency>()
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
